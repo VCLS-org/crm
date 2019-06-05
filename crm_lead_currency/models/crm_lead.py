77dc1eea133f54132b61f943ad2617ee217450fd
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-06-05 16:53:13 +0200

    new implement test

--- crm_lead_currency/models/crm_lead.py
+++ crm_lead_currency/models/crm_lead.py
@@ -30,105 +30,47 @@ class CrmLead(models.Model):
         currency_field='default_currency_id',
     )
 
-    """is_same_currency = fields.Boolean(
-        string='Same currency',
-        compute='_compute_is_same_currency',
-    )"""
 
-    @api.depends('amount_customer_currency')
-    def _compute_default_currency(self):
-        for rec in self:
-            #raise UserError("{}".format(self.env.ref('base.EUR').name))
-            rec.default_currency_id = self.env.ref('base.EUR')
-            #print("GO ROGER")
-
-    @api.onchange('amount_customer_currency','customer_currency_id')
     def get_revenue_in_company_currency(self):
         """
         Compute the planned revenue in the company currency.
         
         If the customer currency is different than the company currency,
         the planned revenue is computed in the company currency.
-        
+        """
         self.ensure_one()
-        #if self.is_same_currency:
-            #return self.planned_revenue
         return self.customer_currency_id._convert(
             self.amount_customer_currency or 0,
-            self.env.ref('base.EUR'),
+            self.default_currency_id or self.env.ref('base.EUR'),
             self.env.user.company_id,
             fields.Datetime.now(),
         )
-        """
+
+    @api.depends('amount_customer_currency')
+    def _compute_default_currency(self):
         for rec in self:
-            rec.planned_revenue = rec.customer_currency_id._convert(
-                rec.amount_customer_currency or 0,
-                rec.default_currency_id or self.env.ref('base.EUR'),
-                self.env.user.company_id,
-                fields.Datetime.now(),
-            )
+            rec.default_currency_id = self.env.ref('base.EUR')
+
+    @api.onchange('amount_customer_currency','customer_currency_id')
+    def _onchange_currency(self):
+        self.planned_revenue = self.get_revenue_in_company_currency()
 
     @api.onchange('partner_id')
     def _onchange_partner(self):
         self.customer_currency_id = self.partner_id.property_product_pricelist.currency_id or self.default_currency_id
-        self.get_revenue_in_company_currency()
-
-    """
-    @api.onchange('customer_currency_id', 'amount_customer_currency')
-    def _onchange_currency(self):
-        self.planned_revenue = self.get_revenue_in_company_currency()
-    
-    @api.multi
-    @api.depends('customer_currency_id', 'company_id.currency_id')
-    def _compute_is_same_currency(self):
-        for lead in self:
-            lead.is_same_currency = False"""
-
-    ################
-    # CRUD Methods #
-    ################
-    
-    #At creation, we force the planned revenue recompute
-    """
-    @api.model
-    def create(self,vals):
-        
-        lead=super().create(vals)
-        lead._onchange_currency()
-
-    """
-
-    """
-
-    @api.onchange('customer_currency_id', 'amount_customer_currency')
-    def _onchange_currency(self):
         self.planned_revenue = self.get_revenue_in_company_currency()
 
-    @api.multi
-    def get_revenue_in_company_currency(self):
-        #Compute the planned revenue in the company currency.
-        #
-        #If the customer currency is different than the company currency,
-        #the planned revenue is computed in the company currency.
-        
-        self.ensure_one()
-        if self.is_same_currency:
-            return self.planned_revenue
-        return self.customer_currency_id._convert(
-            self.amount_customer_currency or 0,
-            self.company_currency,
-            self.env.user.company_id,
-            fields.Datetime.now(),
-        )
 
+    ###############
+    # ORM Methods #
+    ###############
+    
     @api.multi
-    @api.depends('customer_currency_id', 'company_id.currency_id')
-    def _compute_is_same_currency(self):
-        for lead in self:
-            lead.is_same_currency = (
-                lead.customer_currency_id == (
-                    lead.company_currency or
-                    self.env.user.company_id.currency_id
-                )
-            )
-    """
+    def write(self, vals):
+        lead = super().write(vals)
+        # IF planned revenue not converted
+        if 'amount_customer_currency' in vals:
+            if vals['amount_customer_currency'] > 0 and vals['planned_revenue'] == 0:
+                lead.planned_revenue = lead.get_revenue_in_company_currency()
+
+        return lead
